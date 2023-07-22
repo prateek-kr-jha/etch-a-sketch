f8153ed1265e66adb01715690685670afb9548b4
Prateek <prateekj1211@gmail.com>
2023-07-22 20:39:52 +0530

    completed etch a sketch

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 
-function drawGrid(){
+function drawGrid(number){
     let grid_container = document.querySelector(".grid-container");
-    for(let i = 0; i < 16; i++){
+    for(let i = 0; i < number; i++){
         const div = document.createElement("div");
         div.classList.add("column");
-        div.setAttribute("style", `height: 100%; width: ${100/16}%;`);
+        div.setAttribute("style", `height: 100%; width: ${100/number}%;`);
         grid_container.appendChild(div);
     
 
@@ -12,14 +12,35 @@ function drawGrid(){
 
     let columns = document.querySelectorAll(".column");
     for(let j = 0; j < columns.length; j++){
-        console.log(j);
-        for(let k = 0; k < 16; k++){
+        for(let k = 0; k < number; k++){
             const box = document.createElement("div");
             box.classList.add("box");
-            box.setAttribute("style", `height: ${100/16}%; width: 100%;`);
+            box.setAttribute("style", `height: ${100/number}%; width: 100%;`);
             columns[j].appendChild(box);
         }
     }
 }
 
-drawGrid();
+function colorChange(e) {
+    console.log("rajesh")
+    if (e.currentTarget.style.backgroundColor == '') {
+        e.currentTarget.style.backgroundColor = changeColor();
+    }
+}
+
+function changeColor() {
+    let r = Math.floor(Math.random() * 255);
+    let g = Math.floor(Math.random() * 255);
+    let b = Math.floor(Math.random() * 255);
+    currColor = `rgb(${r}, ${g}, ${b})`
+
+    return currColor;
+}
+
+let number = prompt("Enter grid size");
+drawGrid(number);
+let boxes = document.querySelectorAll(".box");
+console.log(boxes);
+boxes.forEach(box => {
+    box.addEventListener("mouseover", colorChange)
+})
